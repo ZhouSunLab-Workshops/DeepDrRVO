9ac47a9e42104ad8607dfe224dc5c6d3759c1621
ZhouSunLab <83796230+ZhouSunLab-Workshops@users.noreply.github.com>
2022-11-17 14:55:02 +0800

    Update DeepDrVBC.py

--- DeepDr/DeepDrVBC.py
+++ DeepDr/DeepDrVBC.py
@@ -1,10 +1,9 @@
 import torch
 from torch import nn
-from tools.utils import get_deepdr
 
 
 class DeepDrVBC(nn.Module):
-    def __init__(self, data_iter=None
+    def __init__(self, data_iter=None, get_deepdr=None
                  ):
         super(DeepDrVBC, self).__init__()
 
